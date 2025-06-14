b0e6f5e45aaa7602842fdb6914d54905e0dcc8f3
rsandu1 <rsandu1@fordham.edu>
2025-06-14 13:51:42 -0400

    Fixed urls.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-                "django-insecure-ccow$tz_=
+                "django-insecure-ccow$tz_="
                 "9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
              )
 
